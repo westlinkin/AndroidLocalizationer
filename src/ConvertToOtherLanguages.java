e5625d94b1635b5c80329f0066edab9143846200
westlinkin <westlinkin@gmail.com>
2014-11-29 15:33:58 +0800

    init project

---
+++ src/ConvertToOtherLanguages.java
@@ -0,0 +1,69 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by Wesley on 11/26/14.
+ */
+public class ConvertToOtherLanguages extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+        final Project project = e.getProject();
+        if (project == null) {
+            return;
+        }
+        Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+        if (editor == null) {
+            return;
+        }
+        final Document document = editor.getDocument();
+        if (document == null) {
+            return;
+        }
+        VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
+        if (virtualFile == null) {
+            return;
+        }
+        final String contents;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(virtualFile.getPath()));
+            String currentLine;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((currentLine = br.readLine()) != null) {
+                stringBuilder.append(currentLine);
+                stringBuilder.append("\n");
+            }
+            contents = stringBuilder.toString();
+        } catch (IOException e1) {
+            return;
+        }
+        final Runnable readRunner = new Runnable() {
+            @Override
+            public void run() {
+                document.setText(contents);
+            }
+        };
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                CommandProcessor.getInstance().executeCommand(project, new Runnable() {
+                    @Override
+                    public void run() {
+                        ApplicationManager.getApplication().runWriteAction(readRunner);
+                    }
+                }, "DiskRead", null);
+            }
+        });
+    }
+}
