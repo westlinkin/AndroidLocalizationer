800adff1d2411f8c40d9dd7ac4b9b0ca4aee5be6
westlinkin <westlinkin@gmail.com>
2014-12-01 16:29:11 +0800

    update copyright

--- src/module/GoogleSupportedLanguages.java
+++ src/module/GoogleSupportedLanguages.java
@@ -1,13 +1,27 @@
-package module;
+/*
+ * Copyright [2014] [Wesley Lin]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import org.apache.velocity.runtime.directive.Foreach;
+package module;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 
 /**
- * Created by Wesley on 11/29/14.
+ * Created by Wesley Lin on 11/29/14.
  */
 public enum GoogleSupportedLanguages {
     Afrikaans("af", "Afrikaans"),
--- src/ui/MultiSelectDialog.java
+++ src/ui/MultiSelectDialog.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright [2014] [Wesley Lin]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ui;
 
-import com.intellij.execution.ui.layout.Grid;
-import com.intellij.execution.ui.layout.GridCell;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.project.Project;
@@ -17,7 +31,6 @@ import com.intellij.ui.mac.foundation.MacUtil;
 import com.intellij.util.Alarm;
 import com.intellij.util.ui.UIUtil;
 import data.StorageDataKey;
-import module.AndroidString;
 import module.GoogleSupportedLanguages;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -29,12 +42,12 @@ import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Created by Wesley on 11/29/14.
+ * Created by Wesley Lin on 11/29/14.
  */
 public class MultiSelectDialog extends DialogWrapper {
 
